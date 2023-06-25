2accfae0f7417e05d3280f97fd57634808eedcd1
luvbigpig <128917254+luvbigpig@users.noreply.github.com>
2023-06-25 23:56:21 +0700

    Update and rename epg.sh to test

--- epg.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-. /etc/profile
-
-
-wget -O $HOME/xmltv.xmltv http://lichphatsong.xyz/schedule/epg.xml
-/usr/bin/socat open:$HOME/xmltv.xmltv UNIX-CONNECT:/var/lib/hts/.hts/tvheadend/epggrab/xmltv.sock
-/usr/bin/socat open:$HOME/xmltv.xmltv UNIX-CONNECT:/home/hts/.hts/tvheadend/epggrab/xmltv.sock
